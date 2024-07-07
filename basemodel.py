19ce786737cd0745ab48cb13f15a51343cbcbcd4
linus <lagbleze@st.ug.edu.gh>
2024-07-08 01:59:20 +0200

    add stem

---
+++ basemodel.py
@@ -0,0 +1,48 @@
+
+#%%
+import torch
+import torch.nn as nn
+from torchvision.models.resnet import conv1x1 #, conv3x3
+#%%
+"""
+Architecture design  -- option for where to bn before activation or after
+option to define activation to use
+option to defice indices to use as identity
+
+Stem block
+takes 12 channels and output 24 channels
+batch norm
+activation
+
+-- fradually increase till 48 channels in output
+
+-- gradually decrease to 1 channel
+-- add indices channels (NDVI and EVI) 
+
+
+learner block
+-- Learner block should take indices not used in stemblock
+
+task block
+"""
+
+def conv3x3(in_channels, out_channels):
+    return nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3)
+
+class StemModel(nn.Module):
+    def __init__(self, in_channels=12, first_out_channel=12, final_out_channels=3):
+        self.conv1 = conv3x3(in_channels=in_channels, out_channels=first_out_channel)
+        self.bn1 = nn.BatchNorm2d(num_features=12)
+        self.relu = nn.ReLU()
+        self.conv2 = conv3x3(in_channels=12, out_channels=first_out_channel*2)
+        self.bn2 = nn.BatchNorm2d(num_features=first_out_channel*2)
+        self.conv3 = conv3x3(in_channels=first_out_channel*2, out_channels=first_out_channel*3)
+        
+        
+        
+    
+    
+    
+
+
+
