78e77d33347104cebffc65fe8ae73ddb8bf36c24
linus <lagbleze@st.ug.edu.gh>
2024-07-19 22:55:32 +0200

    remove unused code

--- field_segment_utils.py
+++ field_segment_utils.py
@@ -23,7 +23,7 @@ def get_tiff_img(path, return_all_bands, bands=("B01", "B03", "B02"),
     
     else:
         band_indexs = [all_band_names.index(band_name) for band_name in bands]
-    #print(band_indexs)
+
     with rasterio.open(path) as src:
         img_bands = [src.read(band) for band in range(1,13)]
     dstacked_bands = np.dstack([img_bands[band_index] for band_index in band_indexs])
